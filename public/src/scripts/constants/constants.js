3ddb5c5188068c4c94c336dd831a1ad017518435
July Sosa <julysosa97@gmail.com>
2018-01-08 19:51:29 -0600

    componer src

--- public/src/scripts/constants/constants.js
+++ public/src/scripts/constants/constants.js
@@ -5,7 +5,7 @@
         .constant('API_URL', 'http://localhost:81/punto_de_venta/public/api/')
         .constant('APP_URL', 'http://localhost:81/punto_de_venta/public/');
 */
-         .constant('WS_URL', 'http://107.170.199.39/punto_de_venta/public/ws/')
-         .constant('API_URL', 'http://107.170.199.39/punto_de_venta/public/api/')
-         .constant('APP_URL', 'http://107.170.199.39/punto_de_venta/public/');
+         .constant('WS_URL', 'http://107.170.199.39/sistemaVentas/public/ws/')
+         .constant('API_URL', 'http://107.170.199.39/sistemaVentas/public/api/')
+         .constant('APP_URL', 'http://107.170.199.39/sistemaVentas/public/');
 }());
